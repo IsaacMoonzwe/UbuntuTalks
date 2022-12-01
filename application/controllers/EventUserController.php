f1b3bb8ca728c258cf52c8d537c18f0397055168
IsaacMoonzwe <117356672+IsaacMoonzwe@users.noreply.github.com>
2022-12-01 17:10:57 +0530

    Update EventUserController.php

--- application/controllers/EventUserController.php
+++ application/controllers/EventUserController.php
@@ -982,7 +982,7 @@ class EventUserController extends MyEventAppController
             $this->set('userData', $userRow);
         }
         $planData = new SearchBase('tbl_three_reasons');
-        $planData->addCondition('registration_plan_title', '=', $method . ".");
+        $planData->addCondition('registration_plan_title', '=', $method);
         $planResult = FatApp::getDb()->fetch($planData->getResultSet());
 
         //  foreach ($sponsorshipList as $key => $value) {
@@ -2897,7 +2897,7 @@ class EventUserController extends MyEventAppController
         }
         $planData = new SearchBase('tbl_three_reasons');
         $planData->addCondition('three_reasons_deleted', '=', 0);
-        $planData->addCondition('registration_plan_title', '=', $planSelected. ".");
+        $planData->addCondition('registration_plan_title', '=', $planSelected);
         $planResult = FatApp::getDb()->fetch($planData->getResultSet());
         $donation_record = new TableRecord('tbl_event_user_ticket_plan');
         $donation_record->assignValues([
